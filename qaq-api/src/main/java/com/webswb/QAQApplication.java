9f6150e28a707b9e61cf9f27f05544d74ff377ba
wangfin <769790863@qq.com>
2023-09-10 23:13:37 +0800

    @wangfin @创建项目框架

---
+++ qaq-api/src/main/java/com/webswb/QAQApplication.java
@@ -0,0 +1,18 @@
+package com.webswb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date 2023/9/6
+ * @author wangfin
+ * @desc 启动类
+ */
+
+@SpringBootApplication()
+public class QAQApplication {
+    public static void main(String[] args){
+        SpringApplication.run(QAQApplication.class, args);
+    }
+}
+
